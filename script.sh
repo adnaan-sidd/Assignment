cebcf628c08d9eeb64b3c4c621068fd791c47475
root <root@ip-172-31-84-14.ec2.internal>
2024-03-29 18:05:23 +0000

    script.sh

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Prompt the user to enter their name
+echo "Hello! What's your name?"
+
+# Read user input into a variable
+read name
+
+# Greet the user
+echo "Nice to meet you, $name!"
+
